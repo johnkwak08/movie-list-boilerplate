9d9630bf7cb2e08480904479f1467ef11e5e6724
John <junyoung.kwak@yahoo.com>
2021-02-18 16:12:23 -0800

    got watched to render properly

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,7 @@ import MovieData from '../MovieData.js';
 import MovieList from './MovieList.jsx';
 import Search from './Search.jsx';
 import Add from './Add.jsx';
+const axios = require('axios');
 
 
 
@@ -17,7 +18,7 @@ class App extends React.Component {
       addedMovies: [],
       searched: false, 
       watched: [], 
-      toWatch: [],
+      toWatch: MovieData,
       watchedIsSelected: false,
       toWatchedIsSelected: false
       
@@ -33,6 +34,30 @@ class App extends React.Component {
     this.onWatchedClick = this.onWatchedClick.bind(this);
     
 
+  }
+
+  componentDidMount() {
+    this.getMovies();
+    
+  }
+  
+  getMovies() {
+    axios.get('https://api.themoviedb.org/3/movie/550?api_key=bbda037578a13e86f5992dc17d514e75')
+      .then((response) => {
+        console.log('Completed', response);
+        console.log(response.data.release_date);
+        // get the response
+        // then map through each object that we receive
+        const newMovies = this.state.movies.map((movie) => 
+          movie.release_date, 
+
+        )
+        // creating a new array of objects containing only key value pairs we need 
+        // setState to new array 
+      })
+      .catch((err) => {
+        console.log('Error', err);
+      })
   }
 
   handleChange(event) {
@@ -78,48 +103,14 @@ class App extends React.Component {
       searched: !this.state.searched})
   }
 
-  switchMovies() {
-    if (this.state.searched) {
-      return (
-        <div>
-          <MovieList 
-          movies={this.state.inputMovies}/>
-        </div>
-      )
-    } else if (this.state.watchedIsSelected) {
-        return (
-          <div>
-            <MovieList 
-            movies={this.state.watched}/>
-          </div>
-        ) 
-    } else if (this.state.toWatchedIsSelected) {
-      return (
-        <div>
-          <MovieList 
-          movies={this.state.toWatch}/>
-        </div>
-      ) 
-    } else {
-        return (
-          <div>
-            <MovieList 
-            movies={this.state.movies}
-            />
-          </div>
-        )
-      }
-      
-  }
-
   onWatchedClick(movie) {
     var data = this.state.movies;
     var watchedMovie = data.filter((movie) => {
       return movie.watched === true;
     })
-    this.setState({watched: watchedMovie})
 
-    this.setState({watchedIsSelected: !this.state.watchedIsSelected})
+    this.setState({watched: watchedMovie, toWatchedIsSelected: false})
+
     console.log('watched' , this.state.watched);
   }
 
@@ -128,9 +119,8 @@ class App extends React.Component {
     var toWatchedMovie = data.filter((movie) => {
       return movie.watched === false;
     })
-    this.setState({toWatch: toWatchedMovie})
+    this.setState({toWatch: toWatchedMovie, toWatchedIsSelected: true} )
 
-    this.setState({watchedIsSelected: !this.state.toWatchedIsSelected})
     console.log('to watched' , this.state.toWatch);
 
   }
@@ -154,8 +144,8 @@ class App extends React.Component {
         </div>
         <button onClick={this.onWatchedClick}>Watched</button>
         <button onClick={this.onToWatchedClick}>To Watch</button>
-        {this.switchMovies()}
-        <div><MovieList movies={this.switchMovies()}/></div>
+        {this.state.toWatchedIsSelected ? <MovieList movies={this.state.toWatch} /> : <MovieList movies={this.state.watched} />}
+        {this.state.searched ? <MovieList movies={this.state.inputMovies} /> : null }
       </div>
 
     )
